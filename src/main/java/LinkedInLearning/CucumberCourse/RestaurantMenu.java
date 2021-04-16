fe61d02dca7539117e157e5212c6a583d0af5e1c
gfike19 <gfike19@gmail.com>
2021-04-16 16:23:50 -0500

    forgot to save RestaurantMenu file

--- src/main/java/LinkedInLearning/CucumberCourse/RestaurantMenu.java
+++ src/main/java/LinkedInLearning/CucumberCourse/RestaurantMenu.java
@@ -1,7 +1,11 @@
 package LinkedInLearning.CucumberCourse;
 
+import java.util.ArrayList;
+
 public class RestaurantMenu {
+	
 	ArrayList<RestaurantMenuItem> MenuItems = new ArrayList<RestaurantMenuItem>();
+	
 	public void Add(RestaurantMenuItem newMenuItem) {
 		// TODO Auto-generated method stub
 		
